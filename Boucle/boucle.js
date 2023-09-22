1527c4941af486ede5f432054745bb9b9d145f15
Azaleeeee <145431299+Azaleeeee@users.noreply.github.com>
2023-09-22 11:54:27 +0200

    Add files via upload

---
+++ Boucle/boucle.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**            BOUCLE
+ *              FOR
+ * Declarer une variable a qui est egale à un entier positif
+ * 
+ * Faire un console log de 'OKLM' autant de fois que le valeur de a
+ * 
+ * 
+ */
+
+let chiffre=2;
+for(let i=0;i<chiffre;i++){
+    console.log('OKLM');
+}
